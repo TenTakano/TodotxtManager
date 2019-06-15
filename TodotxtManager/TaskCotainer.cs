ff883633767dc2da7b6513cab32799964736f790
TenTakano <takano20771@gmail.com>
2019-06-15 19:08:21 +0900

    [Add] R/W tasks commands

---
+++ TodotxtManager/TaskCotainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodotxtManager {
+
+    /// <summary>
+    /// Class for containing a task and its properties
+    /// </summary>
+    public class TaskContainer {
+        public TaskState State { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Add { get; set; }
+
+        public DateTime Finished { get; set; }
+
+        public string Tags { get; set; }
+
+        public string Detail { get; set; }
+
+        public TaskContainer() { }
+
+        public TaskContainer(AddCommand arg) {
+            this.State = TaskState.Todo;
+            this.Name = arg.Name;
+            this.Add = DateTime.Now;
+        }
+
+        override public string ToString() {
+            return $"{this.State}, {this.Name}, {this.Add}, {this.Finished}";
+        }
+    }
+
+    /// <summary>
+    /// Enum for describing state of a task
+    /// </summary>
+    public enum TaskState {
+        Todo,
+        Done,
+        Canceled
+    }
+}
---
+++ TodotxtManager/TaskListManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Reflection;
+
+using TaskList = System.Collections.Generic.List<TodotxtManager.TaskContainer>;
+
+namespace TodotxtManager {
+    public class TaskListManager {
+        private string filePath = Directory.GetParent(Assembly.GetExecutingAssembly().Location)
+                                    + "/todolist.xml";
+        private TaskList taskList = new TaskList();
+
+        public void Add(TaskContainer task) => this.taskList.Add(task);
+
+        public void Show() {
+            if (this.taskList.Count == 0) {
+                Console.WriteLine("There is no tasks");
+            }
+            else {
+                foreach (var task in this.taskList) {
+                    Console.WriteLine(task);
+                }
+            }
+        }
+
+        public void Save() {
+            var serializer = new XmlSerializer(typeof(TaskList));
+            using (var sw = new StreamWriter(this.filePath, false, Encoding.UTF8)) {
+                serializer.Serialize(sw, this.taskList);
+            }
+        }
+
+        public void Load() {
+            if(File.Exists(this.filePath)) {
+                var serializer = new XmlSerializer(typeof(TaskList));
+                using (var sr = new StreamReader(filePath, Encoding.UTF8)) {
+                    this.taskList = (TaskList)serializer.Deserialize(sr);
+                }
+            }
+        }
+    }
+}
